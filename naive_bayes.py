2d046faa0c6bf57968b647c1895ee48038061d70
Michael Estwanick <estwanick@gmail.com>
2018-06-05 22:35:44 -0400

    Implements naive bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,4 +1,5 @@
 import sys
+from math import sqrt
 
 datafile = sys.argv[1]
 f = open(datafile)
@@ -42,8 +43,6 @@ for j in range(0, cols, 1):
     nmFailed.append(0)
     nmSuccess.append(0)
 
-failedLabels = 0
-successLabels = 0
 for i in range(0, rows, 1):
     for j in range(0, cols, 1):
         if(trainLabels.get(i) != None and trainLabels[i] == 1):
@@ -51,32 +50,73 @@ for i in range(0, rows, 1):
         if(trainLabels.get(i) != None and trainLabels[i] == 0):
             nmFailed[j] = nmFailed[j] + data[i][j]
 
-
 for j in range(0, cols, 1):
     nmFailed[j] = nmFailed[j]/labelsSuccess[0]
     nmSuccess[j] = nmSuccess[j]/labelsSuccess[1]
 
-# print(nmFailed)
-# print(nmSuccess)
+#Computing variance
+successVariance = []
+failureVariance = []
+for j in range(0, cols, 1):
+    successVariance.append(0)
+    failureVariance.append(0)
 
-# print labelsSuccess[0]
-# print labelsSuccess[1]
+for i in range(0, rows, 1):
+    for j in range(0, cols, 1):
+        if(trainLabels.get(i) != None and trainLabels[i] == 1):
+            successVariance[j] = (data[i][j] - nmSuccess[j]) ** 2
+        if(trainLabels.get(i) != None and trainLabels[i] == 0):
+            failureVariance[j] = (data[i][j] - nmFailed[j]) ** 2
 
-#Classify points
+print successVariance
+print failureVariance
+
 for i in range(0, rows, 1):
     feature_predictions = {}
     if(trainLabels.get(i) == None):
         d0, d1 = 0, 0
+        nbF = []
+        nbS = []
         for j in range(0, cols, 1):
-            d0 = d0 + ( nmFailed[j] - data[i][j]**2)
-            d1 = d1 + ( nmSuccess[j] - data[i][j]**2)
-            if feature_predictions.get(i) is None:
-                feature_predictions[i] = []    
-            if(d0 < d1):
-                # print('0', i)
-                feature_predictions[i].append('0')
-            else:
-                # print('1', i)
-                feature_predictions[i].append('1')
+            sdF = sqrt(failureVariance[j])
+            sdS = sqrt(successVariance[j])
+            x = data[i][j]
+            nbF.append( ( x - nmFailed[j] ) / sdF )# should sum of all values of nbF and take min
+            nbS.append( ( x - nmSuccess[j] ) / sdS )
+        
+        print min( nbF ), min( nbS )
+        if feature_predictions.get(i) is None:
+            feature_predictions[i] = []    
+        if(d0 < d1):
+            # print('0', i)
+            feature_predictions[i].append('0')
+        else:
+            # print('1', i)
+            feature_predictions[i].append('1')
 
-        print feature_predictions
+        # print feature_predictions
+
+
+
+
+#Classify points
+# for i in range(0, rows, 1):
+#     feature_predictions = {}
+#     if(trainLabels.get(i) == None):
+#         d0, d1 = 0, 0
+#         for j in range(0, cols, 1):
+#             d0 = d0 + ( nmFailed[j] - data[i][j]**2)
+#             d1 = d1 + ( nmSuccess[j] - data[i][j]**2)
+#             if feature_predictions.get(i) is None:
+#                 feature_predictions[i] = []    
+#             if(d0 < d1):
+#                 # print('0', i)
+#                 feature_predictions[i].append('0')
+#             else:
+#                 # print('1', i)
+#                 feature_predictions[i].append('1')
+
+#         print feature_predictions
+
+# Naive Bayes
+
